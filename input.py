3bf4fafcd71ecca0912998ed4dac0cd03c497cf2
omkar murthy <omkarapppu252@gmail.com>
2021-11-15 10:56:32 +0530

    use of comments and user inputs in programming

---
+++ input.py
@@ -0,0 +1,13 @@
+# taking users input in program
+actor = input('enter name:')
+print(actor)
+
+
+# finding out the type of input in program
+number = int(input('value:'))
+print(type(number))
+
+number = float(input('value:'))
+print(type(number))
+
+
