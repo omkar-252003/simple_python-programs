88a1f0bdd9bca28a1a306754c24fae50ca9158ae
omkar murthy <omkarapppu252@gmail.com>
2021-11-24 18:38:36 +0530

    use of operators and their conversion

---
+++ operators.py
@@ -0,0 +1,17 @@
+# discount calculation
+fee = 4023
+discount_percent = 10
+discount_amt = (discount_percent/100) * fee
+net_fee = fee - discount_amt
+print(net_fee)
+print(discount_amt)
+
+# km to miles
+km = 100
+miles = 0.62171
+converted_to_miles = km * miles
+print(converted_to_miles)
+
+# arithmetic operators
+a = (6*8)-(25/5)+ 5+6
+print(a)
